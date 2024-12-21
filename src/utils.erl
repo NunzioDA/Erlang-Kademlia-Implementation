4f0116df029fc2576d76070010054b929dedf670
NunzioDA <nunziodamore@gmail.com>
2024-12-21 21:13:05 +0100

    Updated data handling functions to support binary data instead of arrays

--- src/utils.erl
+++ src/utils.erl
@@ -9,43 +9,47 @@
 
 - export([k_hash/2, get_subtree_index/2, xor_distance/2, sort_node_list/2, map_to_list/1]).
 
-% This function is used to convert a byte into a list of bits.
-byte_to_bit_list(Byte) ->
-    lists:reverse([if
-                     Byte band (1 bsl I) /= 0 -> 1;
-                     true -> 0
-                  end || I <- lists:seq(0, 7)]).
 
-% This function is used to convert a list of bytes into a list of bits.
-to_bit_list(Bytes)->
-    lists:flatten([byte_to_bit_list(Byte) || Byte <- Bytes]).
+% % This function is used to convert a byte into a list of bits.
+% byte_to_bit_list(Byte) ->
+%     lists:reverse([if
+%                      Byte band (1 bsl I) /= 0 -> 1;
+%                      true -> 0
+%                   end || I <- lists:seq(0, 7)]).
+
+% % This function is used to convert a list of bytes into a list of bits.
+% to_bit_list(Bytes)->
+%     HaLi = binary:bin_to_list(Bytes),
+%     lists:flatten([byte_to_bit_list(Byte) || Byte <- HaLi]).
 
 % This function is used to create a K-bit hash from a given data.
-k_hash(Data, K) when is_integer(K), K > 0 -> 
-    Hash = crypto:hash(sha256, Data),   
-    Bit_list = to_bit_list(binary:bin_to_list(Hash)),
-    List = lists:sublist(Bit_list, K),
-    List.
+k_hash(Data, K) when is_integer(K), K > 0, K rem 8 == 0 -> 
+    Hash = crypto:hash(sha256, Data),
+    KBits = binary:part(Hash, {0, ceil(K / 8)}),
+    KBits.
 
 % This function is used to get the index of the subtree that contains the target id.
 get_subtree_index(TargetId, MyId) -> 
-    get_subtree_index(TargetId, MyId, 0).
+    FirstBitIndex = first_different_bit_index(xor_distance(TargetId, MyId)),
+    FirstBitIndex.
+% This function is used to get the index of the first different bit between two binary ids.
+first_different_bit_index(Binary) when is_binary(Binary) ->
+    first_different_bit_index(Binary, 1).
+first_different_bit_index(<<Bit:1, Rest/bits>>, Index) ->
+    case Bit of
+        1 -> 
+            Index;
+        0 -> 
+            first_different_bit_index(Rest, Index + 1)
+    end;
+first_different_bit_index(<<>>, Index) ->
+    Index. 
 
-get_subtree_index([], [], Index) -> 
-    Index;
-get_subtree_index([H|T1], [H|T2], Index) -> 
-    get_subtree_index(T1,T2,Index + 1);
-get_subtree_index([H|_], [H2|_], Index) when H =/= H2 -> 
-    Index.
-    
-% This function is used to calculate the xor distance between two ids.
-xor_distance([], []) -> 0;
-xor_distance([H1|T1], [H2|T2]) ->
-    case H1 =:= H2 of
-        true -> Distance = 0;
-        false -> Distance = math:pow(2,length(T1))
-    end,
-    Distance + xor_distance(T1, T2).
+
+% This function is used to calculate the xor distance between two binary ids.
+xor_distance(HashId1, HashId2) when is_binary(HashId1), is_binary(HashId2) ->
+    Result = lists:zipwith(fun(A, B) -> A bxor B end, binary:bin_to_list(HashId1), binary:bin_to_list(HashId2)),
+    binary:list_to_bin(Result).
 
 % This function is used to sort a list of nodes by their xor distance from a target id.
 sort_node_list(NodeList, TargetId) ->

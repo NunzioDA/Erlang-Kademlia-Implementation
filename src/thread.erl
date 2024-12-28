2db5cf69d706da3d3c5c69c461304634bab504eb
NunzioDA <nunziodamore@gmail.com>
2024-12-28 11:25:46 +0100

    Added the threads verbosity management

--- src/thread.erl
+++ src/thread.erl
@@ -1,11 +1,22 @@
+% -----------------------------------------------------------------------------
+% Module: thread
+% Author(s): Nunzio D'Amore, Francesco Rossi
+% Date: 2024-12-27
+% Description: This module manages the threads behaviour. A thread is considered
+% every process that has been started from another process using the function
+% thread:start, that is considered the thread parent. This makes the thread
+% send messages using the parent address (Pid). 
+% -----------------------------------------------------------------------------
+% 
+
 -module(thread).
 
--export([ start/1]).
+-export([start/1, check_verbose/0, set_verbose/1]).
 
 start(Function) ->
     ParentAddress = com:my_address(),
     Verbose = utils:verbose(),
-    spawn(
+    Pid = spawn(
         fun()->
             % Saving parent address and verbose in the new process
             % so it can behave like the parent
@@ -13,28 +24,45 @@ start(Function) ->
             com:save_address(ParentAddress),
             Function()
         end
-    )
-    % save_thread(Pid)
+    ),
+    save_thread(Pid)
 .
 
-% save_thread(Pid)->
-%     Threads = get_threads(),
-%     put(my_threads, [Pid | Threads])
-% .
-    
-% get_threads() ->
-%     case get(my_threads) of
-%         undefined -> [];
-%         Threads -> Threads
-%     end
-% .
+% This method has to be used in the function
+% passed to the thread to check for verbosity changes
+check_verbose() ->
+    receive
+        {verbose, Verbose} ->
+            utils:set_verbose(Verbose)
+    after 0 ->
+        ok
+    end
+.
 
-% set_verbose(Verbose) ->
-%     Threads = get_threads(),
-%     lists:foreach(
-%         fun(X) ->
-%             gen_server:cast(X, {verbose, Verbose})
-%         end,
-%         Threads
-%     )
-% .
+% This method is used to save the thread Pid in the 
+% Parent process dictionary.
+save_thread(Pid)->
+    Threads = get_threads(),
+    put(my_threads, [Pid | Threads])
+.
+
+% This method is used to get all the threads started
+% from a parent process 
+get_threads() ->
+    case get(my_threads) of
+        undefined -> [];
+        Threads -> Threads
+    end
+.
+
+% This method is called from the parent process to
+% automatically set the verbosity of all its threads 
+set_verbose(Verbose) ->
+    Threads = get_threads(),
+    lists:foreach(
+        fun(Pid) ->
+            Pid ! {verbose, Verbose}
+        end,
+        Threads
+    )
+.

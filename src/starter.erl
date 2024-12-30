b0fd858c39a4cdd7fd485ceb8ffa410d2b2a5fa9
NunzioDA <nunziodamore@gmail.com>
2024-12-31 00:44:52 +0100

    wait_for_network_to_converge bug fix

--- src/starter.erl
+++ src/starter.erl
@@ -8,7 +8,7 @@
 - module(starter).
 
 - export([start/0, registerShell/0, start_kademlia_network/4, test_dying_process/0, pick_random_pid/1,test_join_mean_time/0]).
-- export([wait_for_network_to_converge/0, wait_for_progress/1, destroy/0]).
+- export([wait_for_network_to_converge/1, wait_for_progress/1, destroy/0]).
 % This function is used to start the simulation
 % It starts the analytics_collector and calls
 % the test function to start Bootstraps
@@ -16,7 +16,23 @@
 start() ->    
     analytics_collector:start(),
     ?MODULE:registerShell(),
-    ?MODULE:test_join_mean_time().
+    % ?MODULE:test_join_mean_time()
+    test_join_mean_time()
+
+    % BootstrapNodes = 10,
+    % K = 5,
+    % T = 4000,
+    
+    % StartMillis = erlang:monotonic_time(millisecond),
+    % ?MODULE:start_kademlia_network(BootstrapNodes,250,K,T),
+
+    % utils:print("Waiting for the network to converge~n"),
+    % ?MODULE:wait_for_network_to_converge(),
+
+    % EndMillis = erlang:monotonic_time(millisecond),
+    % Result = EndMillis - StartMillis,
+    % utils:print("~nTotal time for the network with ~p nodes to converge: ~pms~n",[250, Result])
+.
 
 % Join procedure is registered
 % so that all the processes can
@@ -59,7 +75,7 @@ test_dying_process() ->
 
 
     utils:print("Waiting for the network to converge~n"),
-    ?MODULE:wait_for_network_to_converge(),
+    ?MODULE:wait_for_network_to_converge(Nodes),
     
     utils:print("Requiring routing table to the bootstrapnode[~p]...~n",[BootstrapNode]),
     {ok, RoutingTable} = node:get_routing_table(BootstrapNode),
@@ -78,35 +94,64 @@ test_dying_process() ->
     utils:print("New routing table of the bootstrap node [~p] : ~n~p~n",[BootstrapNode,NewRoutingTable])
 .
 
+% get_join_values(X, Y) ->
+%     analytics_collector:start(),
+%     analytics_collector:listen_for(finished_join_procedure),
+%     % BootstrapNodes = 10,
+%     % Nodes = 2000,
+%     K = 5,
+%     T = 4000,
+    
+%     StartMillis = erlang:monotonic_time(millisecond),
+%     ?MODULE:start_kademlia_network(X,Y,K,T),
+
+%     utils:print("Waiting for the network to converge~n"),
+%     ?MODULE:wait_for_network_to_converge(),
+
+%     EndMillis = erlang:monotonic_time(millisecond),
+%     Result = EndMillis - StartMillis,
+%     utils:print("~nTotal time for the network with ~p nodes to converge: ~pms~n",[Y, Result]),
+%     if(Y < 8000) ->
+%         destroy(),
+%         timer:sleep(2000),        
+%         [Result | get_join_values(X*2, Y*2)];
+%     true -> [Result]
+%     end
+% .
+
 test_join_mean_time() ->
     BootstrapNodes = 10,
-    Nodes = 5000,
+    Nodes = 8000,
     K = 5,
     T = 4000,
+
     analytics_collector:listen_for(finished_join_procedure),
     StartMillis = erlang:monotonic_time(millisecond),
     ?MODULE:start_kademlia_network(BootstrapNodes,Nodes,K,T),
+
     utils:print("Waiting for the network to converge~n"),
-    ?MODULE:wait_for_network_to_converge(),
+    ?MODULE:wait_for_network_to_converge(Nodes),
+
     EndMillis = erlang:monotonic_time(millisecond),
     Elapsed = EndMillis - StartMillis,
     utils:print("~nTotal time for the network with ~p nodes to converge: ~pms~n",[Nodes, Elapsed]),
     
     JoinMeanTime = analytics_collector:join_procedure_mean_time(),
-    utils:print("Mean time for processes to join the network: ~pms~n",[JoinMeanTime]),
+    utils:print("Mean time for each processes to join the network: ~pms~n",[JoinMeanTime]),
 
     analytics_collector:flush_join_events(),
     utils:print("~nStarting 5 new nodes to measure join time~n"),
 
+    NewNodes = 5,
     lists:foreach(
         fun(_)->
             node:start(K,T,false)
         end,
-        lists:seq(1,5)
+        lists:seq(1,NewNodes)
     ),
 
     utils:print("Waiting for new nodes to converge~n"),
-    wait_for_network_to_converge(),
+    ?MODULE:wait_for_network_to_converge(NewNodes),
     JoinMeanTimeNewP = analytics_collector:join_procedure_mean_time(),
     utils:print("Mean time for new processes to join the network: ~pms~n",[JoinMeanTimeNewP])
 .
@@ -127,18 +172,32 @@ pick_random_pid(RoutingTable) ->
 %
 % call analytics_collector:listen_for(finished_join_procedure)
 % before calling wait_for_network_to_converge
-wait_for_network_to_converge() ->    
+wait_for_network_to_converge(Started) ->    
     wait_for_progress(
         fun() ->
             Unfinished = analytics_collector:get_unfinished_processes(),
             if length(Unfinished) > 0 ->
                 receive
                     {event_notification, _, _} ->
-                        Started = analytics_collector:get_started_join_processes(),
+
                         Finished = analytics_collector:get_finished_join_processes(),
-                        Progress = length(Finished) / length(Started),
+                        Progress = length(Finished) / Started,
                         Progress
-                after 50000 ->
+                after 8000 ->
+                    % utils:print("MAKE HIM TALK"),
+                    % [First | _] = analytics_collector:get_unfinished_processes(),
+                    % node:talk(First),                    
+                    % Processes = analytics_collector:get_processes_list(),
+                    % Hashes = lists:foldl(
+                    %     fun(E, Acc) ->
+                    %        [utils:to_bit_list(utils:k_hash(E,5))|Acc]
+                    %     end,
+                    %     [],
+                    %     Processes    
+                    % ),
+                    % {ok, File} = file:open("../data/processes.txt", [write]),
+                    % lists:foreach(fun(Pid) -> io:fwrite(File, "~p~n", [Pid]) end, Hashes),
+                    % file:close(File),
                     1
                 end;
             true -> 1
@@ -168,4 +227,7 @@ destroy() ->
         end,
         AllProcesses
     ),
-    analytics_collector:kill().
+    timer:sleep(2000),
+    analytics_collector:kill()
+    % exit(self(),kill)
+.

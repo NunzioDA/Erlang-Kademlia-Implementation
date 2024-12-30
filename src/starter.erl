f1c278b9f6861ea48838e50a31d9f901316bdd36
NunzioDA <nunziodamore@gmail.com>
2024-12-30 13:52:24 +0100

    Added test_join_mean_time

--- src/starter.erl
+++ src/starter.erl
@@ -7,7 +7,7 @@
 
 - module(starter).
 
-- export([start/0, registerShell/0, start_kademlia_network/4, test_dying_process/0, pick_random_pid/1]).
+- export([start/0, registerShell/0, start_kademlia_network/4, test_dying_process/0, pick_random_pid/1,test_join_mean_time/0]).
 - export([wait_for_network_to_converge/0, wait_for_progress/1, destroy/0, flush_finished/0]).
 % This function is used to start the simulation
 % It starts the analytics_collector and calls
@@ -16,7 +16,7 @@
 start() ->    
     analytics_collector:start(),
     ?MODULE:registerShell(),
-    ?MODULE:test_dying_process().
+    ?MODULE:test_join_mean_time().
 
 % Join procedure is registered
 % so that all the processes can
@@ -29,7 +29,9 @@ registerShell() ->
     true-> ok
     end.
 
-start_kademlia_network(Bootstraps, Processes, K,T) ->
+start_kademlia_network(Bootstraps, Nodes, K,T) ->
+    utils:print("Starting a Kademlia network with ~p bootstrap and ~p nodes~n", [Bootstraps,Nodes]),
+    utils:print("~p bit for the hash and ~p millis for republishig~n~n",[K,T]),
     lists:foreach(
         fun(_) ->
             node:start(K, T, true)
@@ -41,22 +43,20 @@ start_kademlia_network(Bootstraps, Processes, K,T) ->
         fun(_) ->
             node:start(K, T, false)
         end,
-        lists:seq(1,Processes)
+        lists:seq(1,Nodes)
     ).
 
 test_dying_process() ->
-    BootstrapNodes = 10,
-    Nodes = 3000,
-    K = 5,
+    BootstrapNodes = 1,
+    Nodes = 5,
+    K = 1,
     T = 4000,
 
     analytics_collector:listen_for(finished_join_procedure),
-    utils:print("Starting a Kademlia network with ~p bootstrap and ~p nodes~n", [BootstrapNodes,Nodes]),
-    utils:print("1 bit for the hash and 4000 millis for republishig~n~n"),
+
     ?MODULE:start_kademlia_network(BootstrapNodes, Nodes, K, T),
     [BootstrapNode|_] = analytics_collector:get_bootstrap_list(),
 
-    node:talk(BootstrapNode),
 
     utils:print("Waiting for the network to converge~n"),
     ?MODULE:wait_for_network_to_converge(),
@@ -77,7 +77,43 @@ test_dying_process() ->
     {ok, NewRoutingTable} = node:get_routing_table(BootstrapNode),
     utils:print("New routing table of the bootstrap node [~p] : ~n~p~n",[BootstrapNode,NewRoutingTable])
 .
+
+test_join_mean_time() ->
+    BootstrapNodes = 10,
+    Nodes = 5000,
+    K = 5,
+    T = 4000,
+    analytics_collector:listen_for(finished_join_procedure),
+    StartMillis = erlang:monotonic_time(millisecond),
+    ?MODULE:start_kademlia_network(BootstrapNodes,Nodes,K,T),
+    utils:print("Waiting for the network to converge~n"),
+    ?MODULE:wait_for_network_to_converge(),
+    EndMillis = erlang:monotonic_time(millisecond),
+    Elapsed = EndMillis - StartMillis,
+    utils:print("~nTotal time for the network with ~p nodes to converge: ~pms~n",[Nodes, Elapsed]),
     
+    JoinMeanTime = analytics_collector:join_procedure_mean_time(),
+    utils:print("Mean time for processes to join the network: ~pms~n",[JoinMeanTime]),
+
+    analytics_collector:flush_join_events(),
+    utils:print("~nStarting a new node to measure join time~n"),
+    StartPMillis = erlang:monotonic_time(millisecond),
+    Pid = node:start(K,T,false),
+    receive
+        {event_notification, _, _} ->
+            EndPMillis = erlang:monotonic_time(millisecond),
+            ElapsedP = EndPMillis - StartPMillis,
+            utils:print("Time for new process to join the network: ~pms~n",[ElapsedP]),
+            timer:sleep(2000),
+            {ok, RoutingTable} = node:get_routing_table(Pid),
+            utils:print("Current routing table of the new node [~p] : ~n~p~n",[Pid,RoutingTable])
+
+
+    after 50000 ->
+        utils:print("Waiting too long for process to join the network. Join process failed.~n")
+    end
+
+.
 
 % This function selects a random pid from the routing table.
 pick_random_pid(RoutingTable) ->
@@ -130,11 +166,14 @@ wait_for_progress(Progress) ->
     end.
 
 destroy() ->
-    AllProcesses = analytics_collector:get_processes_list(),
-    lists:foreach(
-        fun(Pid)->
-            exit(Pid, kill)
-        end,
-        AllProcesses
-    ),
-    analytics_collector:kill().
+    % AllProcesses = analytics_collector:get_processes_list(),
+    % lists:foreach(
+    %     fun(Pid)->
+    %         exit(Pid, kill)
+    %     end,
+    %     AllProcesses
+    % ),
+    % analytics_collector:kill(),
+    % AllTables = ets:all(),
+    % lists:foreach(fun(Table) -> ets:delete(Table) end, AllTables),
+    exit(self(),kill).

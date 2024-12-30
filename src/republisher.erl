64b8e9063097cf6a39d13842efac23094f31dab4
NunzioDA <nunziodamore@gmail.com>
2024-12-30 10:48:54 +0100

    Republisher now uses timer:sleep to wait

--- src/republisher.erl
+++ src/republisher.erl
@@ -22,22 +22,23 @@ start(ValuesTable, RoutingTable, K, T, BucketSize) ->
 % Every T seconds the republisher takes the ValuesMap and foreach 
 % value it starts a store procedure
 republish_behaviour(ValuesTable, RoutingTable, K, T, BucketSize) ->
-    receive
-    after T ->
-        thread:check_verbose(),
-        ValuesMapList = ets:tab2list(ValuesTable),
-        lists:foreach(
-            fun({_, ValuesMap}) ->
-                ValueList = maps:to_list(ValuesMap),
-                lists:foreach(
-                    fun({Key,Value}) ->
-                        node:store_value(Key, Value, RoutingTable, K, BucketSize)
-                    end,
-                    ValueList
-                )
-            end,
-            ValuesMapList
-        ),
-        ?MODULE:republish_behaviour(ValuesTable, RoutingTable, K, T, BucketSize)
-    end
+    timer:sleep(T),
+    % Update verbosity
+    thread:check_verbose(),
+    % Republish every element in the value table
+    ValuesMapList = ets:tab2list(ValuesTable),
+    lists:foreach(
+        fun({_, ValuesMap}) ->
+            ValueList = maps:to_list(ValuesMap),
+            lists:foreach(
+                fun({Key,Value}) ->
+                    node:store_value(Key, Value, RoutingTable, K, BucketSize)
+                end,
+                ValueList
+            )
+        end,
+        ValuesMapList
+    ),
+    % Repeat
+    ?MODULE:republish_behaviour(ValuesTable, RoutingTable, K, T, BucketSize)
 .

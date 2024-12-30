f13ea69bea9488a779ca6327075235bd141b196f
NunzioDA <nunziodamore@gmail.com>
2024-12-30 14:11:59 +0100

    Added check for file modification times to optimize Erlang project compilation by avoiding unnecessary recompilation

--- execution/compiler.erl
+++ execution/compiler.erl
@@ -11,11 +11,35 @@
 
 -module(compiler).
 -export([compile/0]).
+-include_lib("kernel/include/file.hrl").
 
 compile() ->
     {ok, Files} = file:list_dir("../src/"),
     ErlFiles = [filename:join("../src", File) || File <- Files, filename:extension(File) =:= ".erl"],
-    lists:foreach(fun(F) -> compile_file(F) end, ErlFiles).
+    lists:foreach(fun maybe_compile_file/1, ErlFiles).
+
+maybe_compile_file(ErlFile) ->
+    BeamFile = filename:join("../execution", filename:rootname(filename:basename(ErlFile)) ++ ".beam"),
+    case file:read_file_info(ErlFile) of
+        {ok, ErlInfo} ->
+            case file:read_file_info(BeamFile) of
+                {ok, BeamInfo} ->
+                    % Compiling if .erl is modified
+                    if
+                        ErlInfo#file_info.mtime > BeamInfo#file_info.mtime ->
+                            compile_file(ErlFile);
+                        true ->
+                            ok
+                    end;
+                _ ->
+                    % Compiling if beam file doesn't exist
+                    compile_file(ErlFile)
+            end;
+        _ ->
+            io:format("Error reading file info for ~s~n", [ErlFile])
+    end,
+    BeamFile.
 
 compile_file(F) ->
-    io:format("~p~n", [c:c(F)]).
+    io:format("Compiling ~s~n", [F]),
+    c:c(F).

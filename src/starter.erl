219b88096277ae801bede0fede8c28a0133be04a
Francesco Pio Rossi <rossifrancesco020600@gmail.com>
2025-01-13 21:30:16 +0100

    Name of start_kademlia_network changed

--- src/starter.erl
+++ src/starter.erl
@@ -11,7 +11,7 @@
 -export([wait_for_network_to_converge/1, wait_for_progress/1, destroy/0, wait_for_stores/1]).
 -export([choose_test/0, start_test/1,enter_processes_to_kill/1, start_simulation/5, choose_parameters/0]).
 -export([test_dying_process/0, pick_random_pid/1, test_join_mean_time/0,wait_for_progress/2]).
--export([test_lookup_meantime/0, wait_for_lookups/1, test_republisher/0, start_kademlia_network/4]).
+-export([test_lookup_meantime/0, wait_for_lookups/1, test_republisher/0, add_new_nodes/4]).
 
 % This function is used to start the simulation
 % It prints the welcome message and the list of tests
@@ -136,7 +136,7 @@ start_simulation(Bootstraps, Nodes, K, T, EventsToListen) ->
     ),
 
     % Starting the network
-    ?MODULE:start_kademlia_network(Bootstraps, Nodes, K, T),
+    ?MODULE:add_new_nodes(Bootstraps, Nodes, K, T),
 
     utils:print("Waiting for the network to converge~n"),
     TotalNodes = Nodes + Bootstraps,
@@ -145,7 +145,7 @@ start_simulation(Bootstraps, Nodes, K, T, EventsToListen) ->
 .
 
 % This function starts the nodes that will join the kademlia network
-start_kademlia_network(Bootstraps, Nodes, K, T) ->
+add_new_nodes(Bootstraps, Nodes, K, T) ->
 
     case whereis(analytics_collector) of
         undefined ->

1d1b85818e7355939d2a3ffc3f6cf3d47a66557c
NunzioDA <nunziodamore99@gmail.com>
2025-01-26 23:11:00 +0100

    Added K and T parameters for consistency control

--- src/singletons/bootstrap_list_manager.erl
+++ src/singletons/bootstrap_list_manager.erl
@@ -8,14 +8,14 @@
 -module(bootstrap_list_manager).
 
 -export([init/1, handle_call/3, handle_cast/2, enroll_bootstrap/0, get_bootstrap_list/0]).
--export([start/0, location/0, enroll_erl_node/0, get_erl_nodes/0]).
--export([wait_for_initialization/0, kill/0]).
+-export([start/2, location/0, enroll_erl_node/0, get_erl_nodes/0, is_alive/0]).
+-export([wait_for_initialization/0, kill/0,get_simulation_parameters/0]).
 
 -behaviour(singleton).
 
 % This function starts the bootstrap_list_manager
-start() ->
-    singleton:start(?MODULE, [], global)
+start(K, T) ->
+    singleton:start(?MODULE, [K, T], global)
 .
 
 % This function returns the location of the
@@ -53,43 +53,62 @@ get_erl_nodes() ->
     ErlNodesList
 .
 
+% This function is used to make a request
+% to the bootstrap_list_manager to get the current
+% simulation parameters K and T
+get_simulation_parameters() ->
+	singleton:make_request(call, {get_simulation_parameters}, ?MODULE)
+.
+
 % This function initializes the bootstrap_list_manager
-init([ListenerPid]) ->
+init([ListenerPid, K, T]) ->
     singleton:notify_server_is_running(ListenerPid, ?MODULE),
     BootstrapList = [],
     ErlNodeList = [],
-	{ok, {BootstrapList, ErlNodeList}}
+	{ok, {BootstrapList, ErlNodeList, K, T}}
 .
 
 % This function handles the get_bootstrap_list call
 handle_call({get_bootstrap_list}, _, State) ->  
-    {BootstrapList,_} = State,
+    {BootstrapList,_, _, _} = State,
 	{reply, {ok,BootstrapList}, State}
 ;
 % This function handles the get_erl_nodes_list call
 % It returns the list of erlang nodes
 handle_call({get_erl_nodes_list}, _, State) ->  
-    {_,ErlNodes} = State,
+    {_,ErlNodes, _, _} = State,
 	{reply, {ok,ErlNodes}, State}
+;
+% This function is used to handle the requests
+% made to the bootstrap_list_manager requiring 
+% the simulation parameters K and T
+handle_call({get_simulation_parameters}, _From, State) ->
+	{_,_,K,T} = State,
+	{reply, {K,T}, State}
 .
 
 % This function handles the enroll_bootstrap cast
 % It adds the node in From to the list of bootstrap nodes
 handle_cast({enroll_bootstrap,From}, State) ->
-    {BootstrapList,ErlNodeList} = State,
-	{noreply, {[From | BootstrapList], ErlNodeList}}
+    {BootstrapList,ErlNodeList, K, T} = State,
+	{noreply, {[From | BootstrapList], ErlNodeList, K, T}}
 ;
 % This function handles the enroll_erl_node
 % It adds the node to the list of erlang nodes
 % if it is not already present
 handle_cast({enroll_erl_node,Node}, State) ->
-    {BootstrapList,ErlNodeList} = State,
+    {BootstrapList,ErlNodeList, K, T} = State,
     case lists:member(Node, ErlNodeList) of
         true -> 
-            {noreply, {BootstrapList, ErlNodeList}};
+            {noreply, {BootstrapList, ErlNodeList, K, T}};
         false -> 
-            {noreply, {BootstrapList, [Node | ErlNodeList]}}
+            {noreply, {BootstrapList, [Node | ErlNodeList], K, T}}
     end
+.
+
+% This function checks if the analytics collector is alive
+is_alive() ->
+	singleton:is_alive(?MODULE)
 .
 
 % This function kills the bootstrap_list_manager

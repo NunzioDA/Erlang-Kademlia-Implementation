d85937b0739a4a9e42a3013ed41601ce26c04005
NunzioDA <nunziodamore99@gmail.com>
2025-01-26 23:08:58 +0100

    Bug fix destroy now checks if global enviroment is avaliable otherwise it gets local informations. Implemented enviroment_status to get current enviroment status

--- src/kademlia.erl
+++ src/kademlia.erl
@@ -7,19 +7,7 @@
 
 -module(kademlia).
 
--export([start_enviroment/2, start_new_nodes/4, destroy/0, registerShell/0]).
-
-% The shell pid is registered
-% so that all the processes can
-% avoid saving shell pid when 
-% receiving a command from the shell
-registerShell() ->
-    ShellPid = whereis(shellPid),
-    if (ShellPid == undefined) ->
-        register(shellPid, self());
-    true -> ok
-    end
-.
+-export([start_enviroment/2, start_new_nodes/4, destroy/0, registerShell/0, enviroment_status/0]).
 
 % This function is used to start the environment
 % before starting the simulation
@@ -27,23 +15,38 @@ registerShell() ->
 % the shell pid
 start_enviroment(K,T) ->
     ?MODULE:registerShell(),
-    bootstrap_list_manager:start(),
+    % Starting the bootstrap_list_manager
+    bootstrap_list_manager:start(K,T),
     bootstrap_list_manager:wait_for_initialization(),
-    analytics_collector:start(K,T),
+    % Starting the analytics_collector
+    analytics_collector:start(),
     analytics_collector:wait_for_initialization()    
 .
 
+% This function is used to check 
+% the status of the enviroment
+enviroment_status() ->
+    case bootstrap_list_manager:is_alive() of
+        true -> global_enviroment_exists;
+        false -> 
+            case analytics_collector:is_alive() of
+                true -> local_enviroment_exists;
+                false -> no_enviroment
+            end
+    end
+.
 
 % This function starts the nodes that will join the kademlia network
 start_new_nodes(Bootstraps, Nodes, K, T) ->
 
-    case analytics_collector:location() of
-        undefined ->
+    case ?MODULE:enviroment_status() of
+        no_enviroment ->
             ?MODULE:start_enviroment(K, T),
             CanStart = true;
-        _ -> 
+        Status when Status =:= global_enviroment_exists; Status =:= local_enviroment_exists  -> 
             utils:print("An existing enviroment has been found: "),
-            case analytics_collector:get_simulation_parameters() of
+            
+            case bootstrap_list_manager:get_simulation_parameters() of
                 {ExistingK, _} ->
                     if(ExistingK /= K) ->
                         utils:print("~n[ERROR] -> Inconsistent K parameter. ~n"),
@@ -52,6 +55,9 @@ start_new_nodes(Bootstraps, Nodes, K, T) ->
                         utils:print("Please ensure the parameter K is consistent.~n~n"),
                         CanStart = false;
                     true ->
+                        % Starting enviromentto make sure shell is registered
+                        % and analytics_collector is running
+                        ?MODULE:start_enviroment(K, T),
                         utils:print("adding nodes to the network.~n~n"),
                         CanStart = true
                     end;                    
@@ -79,10 +85,35 @@ start_new_nodes(Bootstraps, Nodes, K, T) ->
     end
 .
 
+% The shell pid is registered
+% so that all the processes can
+% avoid saving shell pid when 
+% receiving a command from the shell
+registerShell() ->
+    ShellPid = whereis(shellPid),
+    if (ShellPid == undefined) ->
+        register(shellPid, self());
+    true -> ok
+    end
+.
+
 % This function is used to destroy the simulation
 % It kills all the processes and the analytics_collector
 destroy() ->
-    AllProcesses = lists:flatten(analytics_collector:aggregate_call(get_node_list,[])),
+
+    AllProcesses = 
+        case bootstrap_list_manager:is_alive() of
+            true ->
+                lists:flatten(analytics_collector:aggregate_call(get_node_list,[]));
+            false -> 
+                case analytics_collector:is_alive() of
+                    true ->
+                        analytics_collector:get_node_list();
+                    false ->
+                        []
+                end
+            
+        end,
 
     lists:foreach(
         fun(Pid) ->
@@ -90,9 +121,14 @@ destroy() ->
         end,
         AllProcesses
     ),
-    
-    analytics_collector:aggregate_call(kill,[]),
-    bootstrap_list_manager:kill()
+
+    case bootstrap_list_manager:is_alive() of
+        true ->
+            analytics_collector:aggregate_call(kill,[]),
+            bootstrap_list_manager:kill();
+        false ->
+            analytics_collector:kill()
+    end
     % exit(self(),kill)
 .
 
